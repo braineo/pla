01388364394fc3f23c996120c0fe16d23f62b4fd
Binbin Ye <binbin.ye@mujin.co.jp>
2023-10-26 12:11:48 +0900

    print version diverged dependencies on table
    fix diverged count
    
    fix typo, remove expect
    
    rename types

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,55 @@
 use clap::{value_parser, Arg, Command};
-use log::info;
-use std::path::PathBuf;
+use comfy_table::Table;
+use log::{debug, info, warn, LevelFilter};
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::{HashMap, HashSet},
+    error::Error,
+    fs,
+    path::PathBuf,
+};
+
+#[derive(Debug, Serialize, Deserialize, Default, Clone, Eq, PartialEq)]
+pub struct PackageLockJson {
+    pub name: String,
+    pub version: Option<String>,
+    #[serde(rename = "lockfileVersion")]
+    pub lockfile_version: u32,
+    pub packages: Option<HashMap<String, Dependency>>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq, Default)]
+pub struct Dependency {
+    pub version: String,
+    pub name: Option<String>,
+    pub resolved: Option<String>,
+    pub integrity: Option<String>,
+    #[serde(default)]
+    pub bundled: bool,
+    #[serde(rename = "dev", default)]
+    pub is_dev: bool,
+    #[serde(rename = "optional", default)]
+    pub is_optional: bool,
+    #[serde(rename = "devOptional", default)]
+    pub is_dev_optional: bool,
+    #[serde(rename = "inBundle", default)]
+    pub is_in_bundle: bool,
+    #[serde(rename = "hasInstallScript", default)]
+    pub has_install_script: bool,
+    #[serde(rename = "hasShrinkwrap", default)]
+    pub has_shrink_wrap: bool,
+    pub dependencies: Option<HashMap<String, String>>,
+    #[serde(rename = "devDependencies")]
+    pub dev_dependencies: Option<HashMap<String, String>>,
+    #[serde(rename = "optionalDependencies")]
+    pub optional_dependencies: Option<HashMap<String, String>>,
+    #[serde(rename = "peerDependencies")]
+    pub peer_dependencies: Option<HashMap<String, String>>,
+    pub license: Option<String>,
+    // engines can be map or vec
+    // pub engines: Option<HashMap<String, String>>,
+    pub bin: Option<HashMap<String, String>>,
+}
 
 fn cli() -> Command {
     Command::new("package-lock-analyzer")
@@ -8,19 +57,87 @@ fn cli() -> Command {
         .about("analyze package lock for duplicated packages")
         .arg(
             Arg::new("path")
+                .help("path to packge-lock.json")
                 .value_name("FILE")
                 .value_parser(value_parser!(PathBuf)),
         )
+        .arg(
+            Arg::new("log-level")
+                .help("log level, can be trace, debug, info, warn, error")
+                .short('l')
+                .long("log-level")
+                .value_name("LEVEL")
+                .default_value("info"),
+        )
 }
 
-fn main() {
-    env_logger::builder()
-        .filter_level(log::LevelFilter::Debug)
-        .init();
-
+fn main() -> Result<(), Box<dyn Error>> {
     let matches = cli().get_matches();
+    let mut log_level = log::LevelFilter::Debug;
+
+    if let Some(user_log_level) = matches.get_one::<String>("log-level") {
+        log_level = match user_log_level.as_str() {
+            "trace" => LevelFilter::Trace,
+            "debug" => LevelFilter::Debug,
+            "info" => LevelFilter::Info,
+            "warn" => LevelFilter::Warn,
+            "error" => LevelFilter::Error,
+            _ => LevelFilter::Info,
+        }
+    }
+
+    env_logger::builder().filter_level(log_level).init();
 
     if let Some(package_lock_path) = matches.get_one::<PathBuf>("path") {
         info!("reading package lock from {}", package_lock_path.display());
+        let file = fs::File::open(package_lock_path)?;
+        let lock_file: PackageLockJson = serde_json::from_reader(file)?;
+
+        let mut package_versions: HashMap<String, HashSet<String>> = HashMap::new();
+        match lock_file.packages {
+            Some(packages) => {
+                for (package_install_path, dependency) in packages {
+                    debug!(
+                        "name: {}, version: {}",
+                        package_install_path, dependency.version
+                    );
+
+                    let package_name = package_install_path.rsplit("node_modules/").next().unwrap();
+
+                    let versions = package_versions
+                        .entry(package_name.to_string())
+                        .or_insert(HashSet::new());
+                    versions.insert(dependency.version);
+                }
+            }
+            None => {
+                warn!("no packages to iterate")
+            }
+        }
+
+        let diverged_count: usize = package_versions
+            .iter()
+            .map(|(_, value)| if value.len() > 1 { 1 } else { 0 })
+            .sum();
+
+        info!(
+            "total {} of distinct package installed. {} packages have different versions",
+            package_versions.len(),
+            diverged_count
+        );
+
+        let mut table = Table::new();
+
+        table.set_header(vec!["package", "versions"]);
+
+        for (package_name, versions) in package_versions {
+            if versions.len() > 1 {
+                let mut version_vec = versions.into_iter().collect::<Vec<_>>();
+                version_vec.sort();
+                table.add_row(vec![package_name, version_vec.join(", ")]);
+            }
+        }
+        println!("{table}")
     }
+    Ok(())
 }
