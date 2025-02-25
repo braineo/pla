0d57be0ba28a9188b6109dc3d35deb7f82875e62
Binbin Ye <binbin.ye@mujin.co.jp>
2025-02-25 16:52:56 +0900

    feat(mm2glab): add logging support

--- mm2glab/src/main.rs
+++ mm2glab/src/main.rs
@@ -10,6 +10,11 @@ mod services;
 async fn main() {
     let args = cli::Args::parse();
 
+    env_logger::Builder::from_default_env()
+        .filter_level(args.log_level.into())
+        .format_timestamp_secs()
+        .init();
+
     if let Err(e) = services::run(args).await {
         eprintln!("Error: {}", e);
         process::exit(1);
