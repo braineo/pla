1f7459a6f5b8c5416433efa15b764e864dfa0ea3
Binbin Ye <binbin.ye@mujin.co.jp>
2025-07-31 20:42:33 +0900

    format

--- mm2glab/src/api/mattermost.rs
+++ mm2glab/src/api/mattermost.rs
@@ -1,4 +1,4 @@
-use crate::models::mattermost::{User, Thread, Post};
+use crate::models::mattermost::{Post, Thread, User};
 use anyhow::{Context, Result};
 use async_trait::async_trait;
 use reqwest::{header, Client};
